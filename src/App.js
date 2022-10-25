66bef6b5304df188c9442685982c6a5ee7478d31
Pedro Henrique <pedro@itexto.com.br>
2022-10-25 00:26:49 -0300

    pagina de login, react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Routes from "./routes";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,36 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import "./Login.css";
+
 
 function Login(){
     return (
-        <div className="background">
-            <h1>Login</h1>
-        </div>,
+        <div className="base">
+            <div className="container">
+                <img 
+                    src= "/images/login_icon.png" alt = "Login icon">
+                </img>
+                <p></p>
+                <Form className="inputs">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="rei_da_polenta@gmail.com" />
+                    </Form.Group>
 
-        <div className="name">
-            <h1>Nome Sobrenome</h1>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password" placeholder="eu_amo_polenta" />
+                    </Form.Group>
+                {/* <input></input>
+                <p></p>
+                <input></input> */}
+                </Form>
+                <p></p>
+                <Button className="btn-custom" variant="flat">Entrar</Button>{' '}
+
+            </div>
         </div>
-      
     );
 };
 export default Login;
