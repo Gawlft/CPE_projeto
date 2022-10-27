7355c543383084db4602792777bb19e6f302c2c1
Pedro Henrique <pedro@itexto.com.br>
2022-10-27 10:38:16 -0300

    Página de login finalizada

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,6 +7,7 @@ import "./Login.css";
 function Login(){
     return (
         <div className="base">
+            <div className="login-box">
             <div className="container">
                 <img 
                     src= "/images/login_icon.png" alt = "Login icon">
@@ -15,19 +16,19 @@ function Login(){
                 <Form className="inputs">
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="rei_da_polenta@gmail.com" />
+                        <Form.Control type="email" placeholder="rei_da_polenta@gmail.com"/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password" placeholder="eu_amo_polenta" />
+                        <Form.Control type="password" placeholder="eu_amo_polenta"/>
                     </Form.Group>
-                {/* <input></input>
-                <p></p>
-                <input></input> */}
                 </Form>
                 <p></p>
+                <p></p>
+                <p></p>
                 <Button className="btn-custom" variant="flat">Entrar</Button>{' '}
+                </div>
 
             </div>
         </div>
