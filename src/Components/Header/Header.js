281a21e41771955ed3fc47d007d6003015bafbca
Pedro Henrique <pedro@itexto.com.br>
2022-10-30 16:23:21 -0300

    header changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,14 +2,17 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import "./Header.css";
 
-function Header(){
-    return(
-    <div className="header">
-        <div class="header-right">
-        <Button variant="primary">Home</Button>{' '}
-        <Button variant="light">A Festa</Button>{' '}
+function Header() {
+    return (
+        <div className="header">
+            <div className="header-right">
+                <img
+                    src="/images/pol_icon 1.svg" alt="Plenta icon">
+                </img>
+                <Button variant="primary">Home</Button>{' '}
+                <Button variant="light">A Festa</Button>{' '}
+            </div>
         </div>
-    </div>
     )
 };
 export default Header;
