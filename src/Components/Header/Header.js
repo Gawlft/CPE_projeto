e36027c8a7c2d13d13a2dc5c1207122fa9c8f84e
Pedro Henrique <pedro@itexto.com.br>
2022-10-30 16:42:39 -0300

    header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,13 +5,19 @@ import "./Header.css";
 function Header() {
     return (
         <div className="header">
-            <div className="header-right">
+            <img
+                    src="/images/pol_icon 1.svg" alt="Plenta icon">
+            </img>
+            <Button variant="primary">Home</Button>{' '}
+            <Button variant="light">A Festa</Button>{' '}
+            <span></span>
+            {/* <div className="header-right">
                 <img
                     src="/images/pol_icon 1.svg" alt="Plenta icon">
                 </img>
                 <Button variant="primary">Home</Button>{' '}
                 <Button variant="light">A Festa</Button>{' '}
-            </div>
+            </div> */}
         </div>
     )
 };
