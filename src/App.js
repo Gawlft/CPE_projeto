b75f681408de89a4897f02f073269834ad648484
Pedro Henrique <pedro@itexto.com.br>
2022-10-31 20:40:06 -0300

    global.css created and imported

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Routes from "./routes";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './global.css';
+
 
 function App() {
   return (
