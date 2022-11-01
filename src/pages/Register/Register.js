ed1c3b8e44fcee6933f80a408ba4a97105a3b668
dudalamounier <dudalamou@gmail.com>
2022-11-01 18:56:03 -0300

    register.js clara

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,39 +1,39 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import "./Register.css";
+import "./Register.css"
+import Header from "../../Components/Header/Header"
+import Footer from "../../Components/Footer/Footer"
 
 
-function Login(){
-    return (
-        <div className="base">
-            <div className="login-box">
-            <div className="container">
-                <img 
-                    src= "/images/login_icon.png" alt = "Login icon">
-                </img>
-                <p></p>
-                <Form className="inputs">
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="rei_da_polenta@gmail.com"/>
-                    </Form.Group>
+function Register(){
+    return(
+        <div>
+            <Header/>
+            <div className="register-box">
+                <div className="containerRegister">
+                <img
+                        src="/images/login_icon.png" alt="Login icon">
+                    </img>
+                    <p></p>
+                    <Form className="inputs">
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Label>Nome Completo</Form.Label>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="senha">
+                            </Form.Control>
+                            </Form.Group>
+                            </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password" placeholder="eu_amo_polenta"/>
-                    </Form.Group>
-                </Form>
-                <p></p>
-                <p></p>
-                <Button className="btn-custom" variant="flat">Entrar</Button>{' '}
+                    </Form>
                 </div>
-
             </div>
-            
         </div>
-        
-        
-    );
-};
-export default Login;
+    )
+
+}
+export default Register;
