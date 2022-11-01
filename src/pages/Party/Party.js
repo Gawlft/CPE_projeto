7dac1b3ef3b9e2c39c7ce85fa5144e7fb537720b
dudalamounier <dudalamou@gmail.com>
2022-11-01 18:20:18 -0300

    Festa.js and festa.css created

--- src/pages/Party/Party.js
+++ src/pages/Party/Party.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./Party.css";
+import Header from "../../Components/Header/Header"
+import Footer from "../../Components/Footer/Footer"
 
-function Profile(){
-    return (
-        <div className="background">
-            <h1>Oi</h1>
-        </div>,
+function Party(){
+    return(
+        <div>
+            <Header/>
+        <div className="festaContainer">
+            <div className="festaMain">
+                <h1>A Festa</h1>
+                <div className="eventButtons">
+                <button type="button">Estrutura Física</button>
+                <p></p>
+                <button type="button">2022</button>
+                <p></p>
+                <button type="button">Culinária</button>
+                </div>
+                <img src="/images/polentao.png" alt="Login icon">
 
-        <div className="name">
-            <h1>Nome Sobrenome</h1>
+                </img>
+
+            </div>
         </div>
-      
-    );
-};
+        </div>
 
-export default Profile;
+    )
+}
+export default Party;
