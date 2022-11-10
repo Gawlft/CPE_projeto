d714d9ed9ba00ac7dfa2b20af45703c3a38bae5f
dudalamounier <dudalamou@gmail.com>
2022-11-10 16:41:56 -0300

    Footer pronto e Pagina Party pronto

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,31 @@ import "./Footer.css";
 function Footer(){
 
     return(
-        <h1>to aqui</h1>
-    )
+        <div className="footerBox">
+            <div className="divisoesfooterBox">
+                <h6>EQUIPE</h6>
+                <p>Pedro Cosme</p>
+                <p>Maria Eduarda Lamounier</p>
+                <p>Valentina Santos</p>
+                <p>Maria Bernardes</p>
+            </div>
+            <div className="divisoesfooterBox">
+                <p>Universidade Federal de Minas Gerais</p>
+                <p>Belo Horizonte, Minas Gerais</p>
+                <p>Brasil</p>
+            </div>
+            <div className="divisoesfooterBox">
+                <div className="midiaBox">
+                    <p>@instagram</p>
+                </div>
+                <div className="midiaBox">
+                    <p>CanalYoutube</p>
+                </div>
+                <div className="midiaBox">
+                    <p>31 XXXXX - XXXX</p>
+                </div>
+            </div>
+        </div>
+    )    
 }
 export default Footer;
--- src/pages/Party/Party.js
+++ src/pages/Party/Party.js
@@ -80,27 +80,27 @@ Ferramentas como machado, “grupião”, foice e cangalha faziam parte da rotin
         <div className="culinariaBox">
             <h2>Culinária</h2>
             <div className="culinariaDivisoes">
-                <div className="polentaBox">
+                <div className="alimentoBox">
                     <h3>Polenta</h3>
-                    <img src="/images/polenta frita.png" alt="polenta icon" width="305" height="305"> 
+                    <img src="/images/polenta frita.png" alt="polenta icon" width="300" height="300"> 
                     </img>
                     <h6>A polenta é o principal prato típico da Festa, seja frita, com molho, com ovo, com queijo, com linguiça e outras formas de se apreciar a saborosa iguaria, feita em fogões à lenha com cozimento lento para não queimar e dar paladar</h6>
                 </div>
-                <div className="macarraoBox">
+                <div className="alimentoBox">
                     <h3>Macarrão</h3>
-                    <img src="/images/macarrao.png" alt="macarrao icon" width="305" height="305"> 
+                    <img src="/images/macarrao.png" alt="macarrao icon" width="300" height="300"> 
                     </img>
                     <h6>Também se aprecia um bom macarrão, cozido “al dente” e temperado de forma artesanal, nas versões molho de carne e queijo parmesão e ao "fungui".</h6>
                 </div>
-                <div className="linguiçaBox">
+                <div className="alimentoBox">
                     <h3>Linguiça</h3>
-                    <img src="/images/linguiça.png" alt="linguiça icon" width="305" height="305"> 
+                    <img src="/images/linguiça.png" alt="linguiça icon" width="300" height="300"> 
                     </img>
                     <h6>A linguiça suína é preparada do mesmo modo que nossos "Nonnos" nos ensinaram, bem como o frango frito que é temperado com muito carinho</h6>
                 </div>
-                <div className="capelletiBox">                 
+                <div className="alimentoBox">                 
                     <h3>Capelleti</h3>
-                    <img src="/images/capelleti.png" alt="capelleti icon" width="305" height="305"> 
+                    <img src="/images/capelleti.png" alt="capelleti icon" width="300" height="300"> 
                     </img>
                     <h6>O capeletti, ou agnoline é uma iguaria fina que revigora as forças após uma jornada de boa festa.
 Temos diversas outras porções que agregam sabor ao paladar dos visitantes como um bom queijo, molhos, frango e outros quitutes encontrados no paiol do “Nonno” e na casa da “Nonna", tais como o "socol" e pães caseiros.</h6>
