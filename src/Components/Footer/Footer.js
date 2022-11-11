c163f66880928ca13d652e736c334f014917bbcc
Pedro Henrique <pedro@itexto.com.br>
2022-11-10 22:05:16 -0300

    registerBox e register como componente criado

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,11 +6,13 @@ function Footer(){
     return(
         <div className="footerBox">
             <div className="divisoesfooterBox">
+                <div className="namesFooter">
                 <h6>EQUIPE</h6>
                 <p>Pedro Cosme</p>
                 <p>Maria Eduarda Lamounier</p>
                 <p>Valentina Santos</p>
                 <p>Maria Bernardes</p>
+                </div>
             </div>
             <div className="divisoesfooterBox">
                 <p>Universidade Federal de Minas Gerais</p>
