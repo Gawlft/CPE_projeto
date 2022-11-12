db86dd5e2b5fa74b3b2a1751110bd028afe34ed7
Pedro Henrique <pedro@itexto.com.br>
2022-11-12 11:13:04 -0300

    routes backend

--- cpeBackEnd/src/routes.js
+++ cpeBackEnd/src/routes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const routes = express.Router();
 
+users = [];
 
-routes.get('/home', (req, res) => {
-    console.log(req);
-    res.send("oi");
+routes.get('/users', (req, res) => {
+    res.json(users);
 });
+
+routes.post('/users', (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.json({message: "User created"});
+});
+
 
 module.exports = routes;
