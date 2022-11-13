da5d4cdc50a3180ad01f41039b09281094254cc4
Pedro Henrique <pedro@itexto.com.br>
2022-11-13 19:12:11 -0300

    favoriteAttModel

--- cpeBackEnd/src/models/favoriteAttModel.js
+++ cpeBackEnd/src/models/favoriteAttModel.js
@@ -4,21 +4,24 @@ const connection = require(".../database/connection");
 module.exports = {
     async create(favoriteAtt) {
         const favoriteAtt_id = uuidv4();
-        favoriteAtt.favoriteAtt_id = favoriteAtt_id;
+        favoriteAtt.favoriteProd_id = favoriteAtt_id;
         const result = await connection("favoriteAtt").insert(favoriteAtt);
         return result;
     },
     async getById({ favoriteAtt_id, user_id }) {
-        const result = await connection("favoriteAtt").where({ favoriteAtt_id, user_id }).select("*");
+        const result = await connection("favoriteAtt").where({ favoriteAtt_id, user_id }).select("*").first();
         return result;
 
     },
-    async updateById({favoriteAtt_id, favoriteAtt}){
-        const result = await connection("favoriteAtt").where(favoriteAtt_id).update(favoriteAtt);
-        return result;
+    async getByUserWithFilters(user_id){
+        const result = await connection("favoriteAtt")
+            .innerJoin("attraction", "attraction.attraction_id", "favoriteAtt.attraction_id")
+            .where({"favoriteProdudct.user_id":user_id})
+            .select("attraction.name", "attraction.description");
+            return result;
     },
-    async deleteById(favoriteAtt_id){
-        const result = await connection("favoriteAttProd").where({favoriteAtt_id}).delete();
+    async deleteById(favoriteProd_id){
+        const result = await connection("favoriteAtt").where({favoritefavoriteAtt_id}).delete();
         return result;
     }
 }
