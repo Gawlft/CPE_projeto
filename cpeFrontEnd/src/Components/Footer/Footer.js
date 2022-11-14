57f9557d69a6e13344c3dd14507553135b6321a1
PedroCosme <pcosme555@gmail.com>
2022-11-14 09:47:45 -0300

    footer e profile

--- cpeFrontEnd/src/Components/Footer/Footer.js
+++ cpeFrontEnd/src/Components/Footer/Footer.js
@@ -15,18 +15,26 @@ function Footer(){
                 </div>
             </div>
             <div className="divisoesfooterBox">
+                <img src="/images/enderço.png" alt="endereço icon" width="15" height="15">
+                    </img>
                 <p>Universidade Federal de Minas Gerais</p>
                 <p>Belo Horizonte, Minas Gerais</p>
                 <p>Brasil</p>
             </div>
             <div className="divisoesfooterBox">
                 <div className="midiaBox">
+                    <img src="/images/Instagram.png" alt="instagram icon" width="15" height="15">
+                        </img>
                     <p>@instagram</p>
                 </div>
                 <div className="midiaBox">
+                    <img src="/images/Youtube.png" alt="youtube icon" width="15" height="15">
+                        </img>
                     <p>CanalYoutube</p>
                 </div>
                 <div className="midiaBox">
+                    <img src="/images/Telefone.png" alt="telefone icon" width="15" height="15">
+                        </img>
                     <p>31 XXXXX - XXXX</p>
                 </div>
             </div>
--- cpeFrontEnd/src/pages/Profile/Profile.js
+++ cpeFrontEnd/src/pages/Profile/Profile.js
@@ -7,25 +7,61 @@ function Profile() {
     return (
         <div className="base">
             <Header />
-            <div className="container">
-            </div>
-                <div className="usrname">
-                    <h2>Nome Sobrenome</h2>
-                    <div className="line">
-                    </div>
-                    </div>
-                        <div className="profilepic">
-                            <img src="/images/rainha.jpg" alt="profile">
+            <div className="perfilBox">
+                <div className="infoBox">
+                    <img src="/images/perfilfoto.png" alt="perfil icon" width="266" height="259">
+                        </img>
+                    <div className="informaçoespessoaisBox">
+                        <img src="/images/arroba.png" alt="arroba icon" width="25" height="25">
                             </img>
+                        <img src="/images/Telefone.png" alt="telef icon" width="25" height="25">
+                            </img>
+                        <img src="/images/Bolinho.png" alt="bolo icon" width="25" height="25">
+                            </img>
+                    </div>
+                    <div className="perfilbuttons">
+                        <button type="button">EXCLUIR CONTA</button>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="curiosidadeBox">
+                    <div className="nomeBox">
+                        <h3>Nome</h3>
+                        <h3>Sobrenome</h3>
+                    </div>
+                    <div className="comidasFavoritasBox">
+                        <div className="escolhidosBox">
+                            <img src="/images/garfo.png" alt="garfo icon" width="25" height="25">
+                                </img>
+                            <h2>Comidas Favoritas</h2>
                         </div>
-                        <div className="info">
-                            <div className="info-line"></div>
-                            <span></span>
-                            <div className="info-line"></div>
-                            <span></span>
-                            <div className="info-line"></div>
+                        <div className="perfilbuttons">
+                            <button type="button">Polenta Frita</button>
+                            <p></p>
+                            <button type="button">Capeletti</button>
+                            <p></p>
+                            <button type="button">Linguiça</button>
+                            <p></p>
                         </div>
+                    </div>
+                    <div className="atracoesFavoritas">
+                        <div className="escolhidosBox">
+                            <img src="/images/icon-park-solid_fireworks.png" alt="fireworks icon" width="35" height="35">
+                                </img>
+                            <h2>Atrações Favoritas</h2>
+                        </div>
+                        <div className="perfilbuttons">
+                            <button type="button">Casa da Nonna</button>
+                            <p></p>
+                            <button type="button">Tombo da Polenta</button>
+                            <p></p>
+                            <button type="button">Shows</button>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
 
 
 
