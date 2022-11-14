b353adf5b2607c3e41cc92c372a08e352215fdc2
Pedro Henrique <pedro@itexto.com.br>
2022-11-13 23:47:47 -0300

    firebase

--- cpeBackEnd/src/utils/Firebase.js
+++ cpeBackEnd/src/utils/Firebase.js
@@ -4,7 +4,7 @@ require ('firebase/auth');
 const firebaseConfig={
     apiKey: process.env.APIKEY,
     authDomain: process.env.AUTHDOMAIN,
-    databaseURL: process.env.DATABASEURL,
+   //  databaseURL: process.env.DATABASEURL,
     projectId: process.env.PROJECTID,
     storageBucket: process.env.STORAGEBUCKET,
     messagingSenderId: process.env.MESSAGINGSENDERID,
