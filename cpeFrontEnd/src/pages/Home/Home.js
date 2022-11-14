5842daf4d918503735d6ecebafda86bc0198fec8
valentina <valentinavpsfacul@gmail.com>
2022-11-13 22:03:03 -0300

    home

--- cpeFrontEnd/src/pages/Home/Home.js
+++ cpeFrontEnd/src/pages/Home/Home.js
@@ -10,7 +10,6 @@ function Home(){
       <div>
         <Header/>
         <div className= 'home-container'>
-        {<img src= '/tombodapolenta.jpg'/>}
         <h1>FESTA DA POLENTA</h1>
         <p>Desde 1979 fazendo historia</p>
       </div>
