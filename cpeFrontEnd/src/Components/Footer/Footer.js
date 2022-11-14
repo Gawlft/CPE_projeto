dbaf18325bf86461324c5b1a66e71774561a9726
mariacferreirab <mferreirab2003@gmail.com>
2022-11-14 10:45:43 -0300

    footer espaço

--- cpeFrontEnd/src/Components/Footer/Footer.js
+++ cpeFrontEnd/src/Components/Footer/Footer.js
@@ -15,7 +15,7 @@ function Footer(){
                 </div>
             </div>
             <div className="divisoesfooterBox">
-                <img src="/images/enderço.png" alt="endereço icon" width="15" height="15">
+                <img src="/images/enderço.png" alt="endereço icon" width="19" height="22">
                     </img>
                 <p>Universidade Federal de Minas Gerais</p>
                 <p>Belo Horizonte, Minas Gerais</p>
@@ -23,17 +23,17 @@ function Footer(){
             </div>
             <div className="divisoesfooterBox">
                 <div className="midiaBox">
-                    <img src="/images/Instagram.png" alt="instagram icon" width="15" height="15">
+                    <img src="/images/Instagram.png" alt="instagram icon" width="17" height="17">
                         </img>
                     <p>@instagram</p>
                 </div>
                 <div className="midiaBox">
-                    <img src="/images/Youtube.png" alt="youtube icon" width="15" height="15">
+                    <img src="/images/Youtube.png" alt="youtube icon" width="19" height="17">
                         </img>
                     <p>CanalYoutube</p>
                 </div>
                 <div className="midiaBox">
-                    <img src="/images/Telefone.png" alt="telefone icon" width="15" height="15">
+                    <img src="/images/Telefone.png" alt="telefone icon" width="17" height="17">
                         </img>
                     <p>31 XXXXX - XXXX</p>
                 </div>
