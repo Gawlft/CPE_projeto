71e39beb4b4609f4cf9c3ae4f866f2354f06d278
valentina <valentinavpsfacul@gmail.com>
2022-11-14 16:04:29 -0300

    Components

---
+++ cpeFrontEnd/src/Components/TombodaPolenta/TombodaPolenta.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TombodaPolenta.css";
+
+
+function TombodaPolenta(){
+    <div className="tomboBox">
+                <img src="/images/tombo.png" alt="tombo icon" width="400" height="300"> 
+                </img>
+                <div className="texttombo">
+                    <h2>O Tombo da Polenta</h2>
+                    <p>O 'Tombo da Polenta Gigante', foi um alívio para a cozinha, pois são preparados mais de 1.200 quilos de polenta por vez, abastecendo a cozinha na versão mole e dura para fritar. O tombo da polenta, já foi para o Guinness Book de 2008, e é um ponto atrativo da Festa. Foi criado a partir da ideia do Presidente Tarcísio José Caliman
+</p>
+                </div>
+            </div>
+}
+
+export default TombodaPolenta;
--- cpeFrontEnd/src/services/api.js
+++ cpeFrontEnd/src/services/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import{getToken} from "./auth";
+
 const api = axios.create({
     baseURL: "http://localhost:3333/",
 
