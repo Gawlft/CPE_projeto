7320581906c9393122f56210c7d9c7e3046182bf
PedroCosme <pcosme555@gmail.com>
2022-11-14 01:34:49 -0300

    use state typo

--- cpeFrontEnd/src/pages/Login/Login.js
+++ cpeFrontEnd/src/pages/Login/Login.js
@@ -10,7 +10,7 @@ import "./Login.css";
 
 
 function Login() {
-    const[email, setEmail] = useSate();
+    const[email, setEmail] = useState();
     const[password, setPassword] = useState();
     const navigate = useNavigate();
 
