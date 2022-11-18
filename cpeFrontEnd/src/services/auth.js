5d57c924e667c6225dfd062c2336930b543ebf4e
PedroCosme <pcosme555@gmail.com>
2022-11-18 09:15:28 -0300

    user id get

--- cpeFrontEnd/src/services/auth.js
+++ cpeFrontEnd/src/services/auth.js
@@ -1,5 +1,6 @@
 export const TOKEN_KEY = "@polenta-Token";
 export const USER_EMAIL = "@email";
+export const USER_ID = "@id";
 
 export const isAuthenticated = () => sessionStorage.getItem(TOKEN_KEY) !== null;
 
@@ -16,4 +17,11 @@ export const setSession = email => {
 sessionStorage.setItem(USER_EMAIL, email);
 };
 
-export const getSession = () => sessionStorage.getItem(USER_EMAIL);
+export const getSession = () =>{sessionStorage.getItem(USER_EMAIL); }
+
+export const setUserId = user => {
+  sessionStorage.setItem(USER_ID, user);
+  };
+  
+export const getUserId = () => { sessionStorage.getItem(USER_ID); }
+  x'
